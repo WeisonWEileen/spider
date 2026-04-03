a9070a12c9edb361bfe29ea14252261d40b10251
weisonweileen <weisonweileen@gmail.com>
2026-04-04 02:29:04 +0800

    update sharpa xml file
    Signed-off-by: weisonweileen <weisonweileen@gmail.com>

--- run.sh
+++ run.sh
@@ -20,15 +20,38 @@ uv run spider/preprocess/decompose_fast.py --task=${TASK} --dataset-name=${DATAS
 uv run spider/preprocess/detect_contact.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE} --no-show-viewer
 
 # # # generate scene
-uv run spider/preprocess/generate_xml.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE} --robot-type=${ROBOT_TYPE} --no-show-viewer
+# uv run spider/preprocess/generate_xml.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE} --robot-type=${ROBOT_TYPE} --no-show-viewer
 
-# # # kinematic retargeting
-# # # here we use fast IK pipeline with mink=
-# # # you can also use ik.py for original ik pipeline with mujoco (used in paper)
-uv run spider/preprocess/ik_fast.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE} --robot-type=${ROBOT_TYPE}
+# # # # kinematic retargeting
+# # # # here we use fast IK pipeline with mink=
+# # # # you can also use ik.py for original ik pipeline with mujoco (used in paper)
+# uv run spider/preprocess/ik_fast.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE} --robot-type=${ROBOT_TYPE}
 
-# # retargeting
-uv run examples/run_mjwp.py +override=${DATASET_NAME} task=${TASK} data_id=${DATA_ID} robot_type=${ROBOT_TYPE} embodiment_type=${HAND_TYPE}
+# # # retargeting
+# uv run examples/run_mjwp.py +override=${DATASET_NAME} task=${TASK} data_id=${DATA_ID} robot_type=${ROBOT_TYPE} embodiment_type=${HAND_TYPE}
 
-# # read data for deployment (optional)
+# # # read data for deployment (optional)
 # uv run spider/postprocess/read_to_robot.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --robot-type=${ROBOT_TYPE} --embodiment-type=${HAND_TYPE}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ensure sync
+cp example_datasets/processed/gigahand/assets/robots/sharpa/bimanual.xml spider/assets/robots/sharpa/bimanual.xml
+cp example_datasets/processed/gigahand/assets/robots/sharpa/left.xml spider/assets/robots/sharpa/left.xml
+cp example_datasets/processed/gigahand/assets/robots/sharpa/right.xml spider/assets/robots/sharpa/right.xml
