bea9b1eefb0c7eb8440e9a015601f50d020a8483
weisonweileen <weisonweileen@gmail.com>
2026-04-04 01:08:41 +0800

    update mjwrap version
    Signed-off-by: weisonweileen <weisonweileen@gmail.com>

--- run.sh
+++ run.sh
@@ -9,22 +9,22 @@ DATASET_NAME=gigahand
 # put your raw data under folder raw/{dataset_name}/ in your dataset folder
 
 # read data from self collected dataset
-# uv run spider/process_datasets/gigahand.py --participant=${PARTICIPANT} --embodiment-type=${HAND_TYPE} --sequence-id=$(printf '%04d' ${DATA_ID}) --no-show-viewer
+uv run spider/process_datasets/gigahand.py --participant=${PARTICIPANT} --embodiment-type=${HAND_TYPE} --sequence-id=$(printf '%04d' ${DATA_ID}) --no-show-viewer
 
 # # decompose object
 # # here we use fast decompose pipeline with mink
 # # you can also use decompose.py for original decompose pipeline with CoACD for higher quality decomposition
-# uv run spider/preprocess/decompose_fast.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE}
+uv run spider/preprocess/decompose_fast.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE}
 
 # # detect contact (optional)
-# uv run spider/preprocess/detect_contact.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE} --no-show-viewer
+uv run spider/preprocess/detect_contact.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE} --no-show-viewer
 
 # # # generate scene
 uv run spider/preprocess/generate_xml.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE} --robot-type=${ROBOT_TYPE} --no-show-viewer
 
-# # kinematic retargeting
-# # here we use fast IK pipeline with mink=
-# # you can also use ik.py for original ik pipeline with mujoco (used in paper)
+# # # kinematic retargeting
+# # # here we use fast IK pipeline with mink=
+# # # you can also use ik.py for original ik pipeline with mujoco (used in paper)
 uv run spider/preprocess/ik_fast.py --task=${TASK} --dataset-name=${DATASET_NAME} --data-id=${DATA_ID} --embodiment-type=${HAND_TYPE} --robot-type=${ROBOT_TYPE}
 
 # # retargeting
